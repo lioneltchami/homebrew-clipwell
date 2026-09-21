744c393e97ebc2384611248771a83b58b6d85131
Lionel Tchami <lionel@getclipwell.com>
2026-09-21 12:54:38 -0600

    feat: add clipwell cask (v1.1.0)

---
+++ Casks/clipwell.rb
@@ -0,0 +1,24 @@
+cask "clipwell" do
+  version "1.1.0"
+  sha256 "49a63ed414ee2fcf77a1d1b280842cf5e8418f3ebd5cc74caa7b5f24ea65a3a5"
+
+  url "https://github.com/lioneltchami/clipwell/releases/download/v#{version}/Clipwell-#{version}-arm64.dmg"
+  name "Clipwell"
+  desc "Native macOS menu-bar screen recorder"
+  homepage "https://lioneltchami.github.io/clipwell/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :sequoia"
+
+  app "Clipwell.app"
+
+  zap trash: [
+    "~/Library/Application Support/Clipwell",
+    "~/Library/Preferences/com.lionel.Clipwell.plist",
+    "~/Library/HTTPStorages/com.lionel.Clipwell",
+  ]
+end
